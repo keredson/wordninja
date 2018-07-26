7b5a4285e9cab84b53f9d62f92f12598808e44e5
Derek Anderson <public@kered.org>
2018-07-26 11:04:35 -0700

    caps

--- test.py
+++ test.py
@@ -13,6 +13,8 @@ class TestWordNinja(unittest.TestCase):
     self.assertEqual(list(wordninja.split('derek_anderson')), ['derek','anderson'])
     self.assertEqual(list(wordninja.split('derek/anderson')), ['derek','anderson'])
 
+  def test_caps(self):
+    self.assertEqual(list(wordninja.split('DEREKANDERSON')), ['derek','anderson'])
 
 if __name__ == '__main__':
     unittest.main()
